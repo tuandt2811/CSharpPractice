b8c8525de6b33746b9eaf0bcf5a73fdeae572f97
tuandam <tuan.trong.dam@nxp.com>
2018-09-24 18:42:41 +0700

    Update source code for generic

--- Chapter14_Practice/Chapter14_Practice/Generic.cs
+++ Chapter14_Practice/Chapter14_Practice/Generic.cs
@@ -5,7 +5,55 @@ using System.Text;
 
 namespace Chapter14_Practice
 {
-    class Generic
+    class GenericList<T> where T : IComparable
     {
+        private const int DefaultElements = 100;
+        private int NumberOfElement;
+        private T[] Array;
+
+        /* Constructors */
+        public GenericList() : this(DefaultElements)
+        {
+
+        }
+
+        public GenericList(int NumElements)
+        {
+            this.NumberOfElement = NumElements;
+            this.Array = new T[this.NumberOfElement];
+        }
+
+        /* Methods */
+        /* 1. Add an item with index */
+        public void AddItem(int Index, T value)
+        {
+            if (Index < this.NumberOfElement)
+            {
+                if(this.Array[Index].CompareTo(value) == 0)
+                {
+                    this.Array[Index] = value;
+                }
+                else
+                {
+                    /* Through an exception */
+                }
+            }
+            else
+            {
+                /* Through an exception */
+            }
+        }
+        /* 2. Remove an item with index */
+        public void RemoveItem(int Index)
+        {
+
+        }
+        /* 3. Insert an item with index */
+
+        /* 4. Clear the List */
+
+        /* 5. Search an item by value */
+
+        /* 6. Override the method ToString */
     }
 }
